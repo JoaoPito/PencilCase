060e2882abf1051a4ff17f761b7a0c847dd3dbdd
JoaoPito <joao.pito@hotmail.com>
2024-08-01 19:08:31 -0300

    Refactored FragmentAPI service

--- PencilCase.Web/Models/FragmentGenerator.cs
+++ PencilCase.Web/Models/FragmentGenerator.cs
@@ -1,26 +1,8 @@
-using System.ComponentModel.DataAnnotations;
-using System.Dynamic;
-using System.Text.Json.Serialization;
-using PencilCase.Shared.Models.StudyGuide;
-
 namespace PencilCase.Web.Models;
 
-public class FragmentGenerator : Fragment
+public record FragmentGenerator
 {
-    public FragmentGenerator() : base()
-    {
-        Endpoint = String.Empty;
-        Description = String.Empty;
-    }
-    public FragmentGenerator(String name, 
-        String content, 
-        String description,
-        String endpoint) : base(name, content)
-    {
-        Endpoint = endpoint;
-        Description = description;
-    }
-
-    public string Description { get; set; }
-    public string Endpoint { get; set; }
+    public string Name { get; set; } = String.Empty;
+    public string Description { get; set; } = String.Empty;
+    public string Endpoint { get; set; } = String.Empty;
 }
--- PencilCase.Web/Services/FragmentAPI.cs
+++ PencilCase.Web/Services/FragmentAPI.cs
@@ -2,58 +2,56 @@ using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
 using PencilCase.Shared.Models;
-using PencilCase.Shared.Models.StudyGuide;
-using PencilCase.Web.Components;
 using PencilCase.Web.Models;
-using PencilCase.Web.Models.Responses;
+using PencilCase.Web.Models.Responses.StudyGuide;
 
 namespace PencilCase.Web.Services;
 
-public class FragmentAPI
+public class FragmentApi
 {
-    private HttpClient _httpClient;
-    private List<FragmentEndpoint>? _fragments = null;
+    private readonly HttpClient _httpClient;
 
-    public FragmentAPI(IHttpClientFactory httpClientFactory)
+    public FragmentApi(IHttpClientFactory httpClientFactory)
     {
         _httpClient = httpClientFactory.CreateClient("API");
     }
-    public async Task<List<FragmentEndpoint>?> GetAvailableFragments(){
-        if(_fragments == null || _fragments.Count == 0)
-            _fragments = await _httpClient.GetFromJsonAsync<List<FragmentEndpoint>>("\\");
-        return _fragments;
+    public async Task<List<FragmentGenerator>?> GetAllGenerators()
+    {
+        var responses = await _httpClient.GetFromJsonAsync<List<FragmentResponse>>("\\");
+        if (responses == null)
+            throw new NullReferenceException("Response is null!");
+        return MapResponsesToInvocableFragments(responses);
     }
 
-    public async Task<Fragment> GenerateFragment(string name, string topic){
-        await GetAvailableFragments();
-        EnsureHasFragments();
-        var requestedFragment = GetFragmentEndpointBy(f => f.Name == name);
-
+    public async Task<Fragment> GenerateFragment(FragmentGenerator fragmentGenerator, string topic){
         var requestContent = GetGenerationRequest(topic);
-        var response = await _httpClient.PostAsync(requestedFragment.Endpoint, requestContent);
+        var response = await _httpClient.PostAsync(fragmentGenerator.Endpoint, requestContent);
         response.EnsureSuccessStatusCode();
-        var responseObj = await JsonSerializer.DeserializeAsync<SGApiResponse>(await response.Content.ReadAsStreamAsync());
+        var responseObj = await JsonSerializer.DeserializeAsync<InvokeResponse>(await response.Content.ReadAsStreamAsync());
 
         if (responseObj is null)
         {
             throw new NullReferenceException();
         }
 
-        return MapResponseToFragment(requestedFragment, responseObj);
+        return new Fragment{
+            Name = fragmentGenerator.Name, 
+            Content = responseObj.Output
+        };
+    }
+    
+    private FragmentGenerator MapResponseToInvocableFragment(FragmentResponse response)
+    {
+        return new FragmentGenerator{
+            Name = response.Name, 
+            Description = response.Description,
+            Endpoint = response.Endpoint
+        };
     }
 
-    private void EnsureHasFragments(){
-        if(_fragments == null || _fragments.Count == 0)
-            throw new ArgumentException("Could not load any fragments");
-    }
-
-    private FragmentEndpoint GetFragmentEndpointBy(Func<FragmentEndpoint, bool> criteria){
-        EnsureHasFragments();
-        var requestedFragment = _fragments.FirstOrDefault(criteria);
-
-        if(requestedFragment == null)
-            throw new ArgumentException("Could not find requested fragment");
-        return requestedFragment;
+    private List<FragmentGenerator> MapResponsesToInvocableFragments(List<FragmentResponse> responses)
+    {
+        return responses.Select(r => MapResponseToInvocableFragment(r)).ToList();
     }
 
     private StringContent GetGenerationRequest(string topic)
@@ -71,8 +69,5 @@ public class FragmentAPI
         );
     }
 
-    private Fragment MapResponseToFragment(FragmentEndpoint fragment,SGApiResponse response)
-    {
-        return new Fragment(fragment.Name, response.Output);
-    }
+    
 }
