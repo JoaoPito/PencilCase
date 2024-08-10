e10c84a9186121176626836b863f5cc5167cd2c0
JoaoPito <joao.pito@hotmail.com>
2024-08-10 15:55:03 -0300

    Created StudyGuideToolbarActions

---
+++ PencilCase.Web/StudyGuideGenerator/Models/StudyGuideToolbarActions.cs
@@ -0,0 +1,9 @@
+namespace PencilCase.Web.StudyGuideGenerator.Models;
+
+public record StudyGuideToolbarActions
+{
+    public Action<String>? OnRegenerate { get; set; } 
+    public Action<String>? OnDelete { get; set; } 
+    public Action<String>? OnShare { get; set; } 
+
+}
