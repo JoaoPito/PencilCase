657da99b97df494e8f259f6a465a84366ad22bff
JoaoPito <joao.pito@hotmail.com>
2024-08-12 21:58:29 -0300

    Fixed formatting for MarkdownExporter

--- PencilCase.Shared.Files/FileExporters/MarkdownExporter.cs
+++ PencilCase.Shared.Files/FileExporters/MarkdownExporter.cs
@@ -9,15 +9,35 @@ public class MarkdownExporter : TextFileExporter
         var filename = studyGuide.Topic + studyGuide.LastOpened
                                             .ToLocalTime()
                                             .ToString("dd_MM_yyyy");
-        var file_contents = $"# {studyGuide.Topic}";
+        var file_contents = FormatTitle(studyGuide.Topic);
 
         foreach(var fragment in studyGuide.Fragments){
-            var sectionTitle = $"## {fragment.Name}\n";
+            var sectionTitle = FormatSubtitle(fragment.Name);
             var sectionContent = $"{fragment.Content}\n";
             file_contents += sectionTitle + sectionContent;
         }
 
         MarkdownFile file = new MarkdownFile(filename, file_contents);
         return file;
+    }
+
+    protected String CapitalizeText(String text){
+        if (string.IsNullOrEmpty(text))
+            return text;
+        string[] words = text.Split(' ');
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1).ToLower();
+        }
+        return string.Join(" ", words);
+    }
+
+    protected String FormatTitle(String title){
+        return $"# {CapitalizeText(title)}\n";
+    }
+
+    protected String FormatSubtitle(String subtitle) {
+        return $"## {CapitalizeText(subtitle)}\n";
     }
 }
