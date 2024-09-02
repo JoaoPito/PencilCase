d1404eeaf948e7498b123440025d35287e85680f
JoaoPito <joao.pito@hotmail.com>
2024-09-02 18:56:54 -0300

    llm-service: Changed NextStepsGenerator prompt

--- llm-service/app/generators/nextsteps.py
+++ llm-service/app/generators/nextsteps.py
@@ -12,18 +12,19 @@ class NextStepsGenerator(Generator):
 Write 1 to 3 ideas of next topics to be explored after a bachelor's course in '{topic}'. Those topics need to be similar and in the order that a good university course generally presents in a syllabus.
 The topics need to be related but can also include subjects of other, related, courses.
 Write the title of the field and a very short 1-line description.
-IF the student asks for something that is NOT a valid topic write the message "TOPIC NOT VALID".
+IF the student asks for something that is NOT a valid study topic write the message "TOPIC NOT VALID".
 WRITE ONLY THE TOPICS AND NOTHING ELSE.
 NEVER WRITE ANY TITLES STARTING WITH '#'.
 ALWAYS WRITE SOMETHING.
 ALWAYS WRITE IN THE STUDENT'S LANGUAGE.
 # EXAMPLE
 Topic: "Computer Science"
-Next Topics:
+
 - **Artificial Intelligence:** Development of intelligent agents that can reason, learn, and act autonomously.
 - **Machine Learning:** Algorithms that allow computers to learn from data and improve their performance over time.
 - **Data Science:** Extraction of insights from large datasets using statistical and computational techniques.
 # NEXT TOPICS
+Topic: "{topic}"
 """)
     
     model = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=config("GOOGLE_API_KEY"))
