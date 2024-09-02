599283569be2d9841e2829d165cd8658741546fd
JoaoPito <joao.pito@hotmail.com>
2024-09-02 17:08:12 -0300

    llm-service: Updated Introduction generator

--- llm-service/app/generators/introduction.py
+++ llm-service/app/generators/introduction.py
@@ -6,13 +6,14 @@ from decouple import config
 class IntroductionGenerator(Generator):
     name = 'Introduction'
     endpoint = '/intro'
-    description = 'Add an introduction to the topic, explaining briefly basic concepts'
+    description = 'Gives a starting point briefly explaining the basic aspects of the topic.'
 
     prompt = ChatPromptTemplate.from_template("""You are a world-class professor in a prestigious university. 
 Write a basic and easy to understand introduction to the topic '{topic}'. 
 It should have at most 1 paragraph and should cover the basic aspects of '{topic}' and nothing else.
 IF the student asks for something that is not a valid topic write the message "TOPIC NOT VALID".
-WRITE ONLY THE INTRODUCTION AND NOTHING ELSE. DO NOT WRITE ANY TITLES STARTING WITH '#'. ALWAYS WRITE SOMETHING.
+WRITE ONLY THE INTRODUCTION AND NOTHING ELSE. DO NOT WRITE ANY TITLES STARTING WITH '#'. ALWAYS WRITE SOMETHING. ALWAYS WRITE IN THE STUDENT'S LANGUAGE.
+
 # EXAMPLE
 Computer science is the study of computers and how they work. It's not just about using computers, but about understanding the underlying principles that make them tick. This includes things like how computers store and process information, how they communicate with each other, and how they are programmed to perform specific tasks. 
 It is a vast field with many different areas of specialization. Some common areas include software engineering (creating software programs), artificial intelligence (teaching computers to think like humans), data science (analyzing large amounts of data), and computer security (protecting computer systems from attacks).
