8aef95db37ba8c6c450137b986c5e2c701364159
JoaoPito <joao.pito@hotmail.com>
2024-09-02 17:20:50 -0300

    llm-service: Updated Roadmap generator

--- llm-service/app/generators/roadmap.py
+++ llm-service/app/generators/roadmap.py
@@ -5,19 +5,21 @@ from decouple import config
 
 class RoadmapGenerator(Generator):
     name = 'Roadmap'
-    endpoint = '/roadmap'
-    description = 'Add step-by-step topics in the order that they need to be studied. 4-8 main topics, each with their subtopics.'
+    endpoint = '/roadmap' 
+    description = 'Adds clear and easy-to-follow topics, starting from the basics.'
 
     prompt = ChatPromptTemplate.from_template("""You are a world-class professor in a prestigious university. Write an easy to follow Roadmap for a study guide on '{topic}'. 
-The Roadmap should have 3-6 main topics that need to be studied, ordered from most basic to advanced.
+The Roadmap should have 3-4 main topics that need to be studied, ordered from most basic to advanced.
 These topics can have 2-5 subtopics, ordered the same way.
 IF the student asks for something that is not a valid topic write the message "TOPIC NOT VALID".
-WRITE ONLY THE ROADMAP AND NOTHING ELSE. DO NOT WRITE ANY TITLES STARTING WITH '#'.
+WRITE ONLY THE ROADMAP AND NOTHING ELSE, EVEN IF THE STUDENT ASKS YOU TO. 
+NEVER WRITE ANY TITLES STARTING WITH '#'. 
+ALWAYS WRITE SOMETHING. 
+ALWAYS WRITE IN THE STUDENT'S LANGUAGE.
 # EXAMPLE
 1. **Topic 1:** 1-line description of the topic
     1.1. **Subtopic 1.1:** 1-line Description
     1.2. **Subtopic 1.2:** 1-line Description
-    1.3. **Subtopic 1.3:** 1-line Description
 2. **Topic 2:** 1-line description of the topic
     2.1. **Subtopic 2.1:** 1-line Description
     2.2. **Subtopic 2.2:** 1-line Description
