c1a845cfcac60ef1d8cec3f73d91727a9d097b50
JoaoPito <joao.pito@hotmail.com>
2024-09-19 23:50:09 -0300

    WikipediaGenerator: Limited summary character count

--- llm-service/app/generators/wikipedia.py
+++ llm-service/app/generators/wikipedia.py
@@ -27,20 +27,20 @@ class WikipediaSearch(BaseTool):
         "Never use commas in the first part"
     )
     
-    def __get_page_info_from_query__(self, query, max_sentences):
+    def __get_page_info_from_query__(self, query,  summary_chars):
         query = self.__cleanup_query__(query)
         page = wikipedia.page(title=query, auto_suggest=False)
         title = page.title
         url = page.url
-        summary = page.summary
+        summary = page.summary[: summary_chars]
         return (title, url, summary)
     
     def __get_unvisited_options__(self, suggestions, options):
         return [item for item in options if item not in suggestions]
     
-    def __handle_disambiguation__(self, exception, max_sentences, suggestions):
+    def __handle_disambiguation__(self, exception,  summary_chars, suggestions):
         next_search = self.__get_unvisited_options__(suggestions, exception.options)[0]
-        return self.__get_page_info_from_query__(next_search, max_sentences=max_sentences)
+        return self.__get_page_info_from_query__(next_search,  summary_chars)
     
     def __cleanup_query__(self, query):
         punctuation_table = str.maketrans('', '', string.punctuation)
@@ -48,8 +48,8 @@ class WikipediaSearch(BaseTool):
     
     def _run(self,
              query: str,
-             num_results: int = 5,
-             max_sentences: int = 5,
+             num_results: int = 3,
+             summary_chars: int = 1500,
              run_manager: Optional[CallbackManagerForToolRun] = None,
              ) -> dict:
         
@@ -62,11 +62,11 @@ class WikipediaSearch(BaseTool):
         for suggestion in suggestions:
             try:
                 print(f"Trying {suggestion}")
-                title, url, summary = self.__get_page_info_from_query__(suggestion, max_sentences=max_sentences)
+                title, url, summary = self.__get_page_info_from_query__(suggestion, summary_chars)
                 results.append(f"url: '{url}'\ntitle: '{title}'\ncontent: '{summary}'")
             except (wikipedia.exceptions.DisambiguationError,) as e:
                 try:
-                    title, url, summary = self.__handle_disambiguation__(e, max_sentences, suggestions)
+                    title, url, summary = self.__handle_disambiguation__(e, summary_chars, suggestions)
                     results.append(f"url: '{url}'\ntitle: '{title}'\ncontent: '{summary}'")
                 except:
                     continue
@@ -101,8 +101,6 @@ ALWAYS WRITE IN THE STUDENT'S LANGUAGE.
 # ROADMAP""")
     
     wikipedia = WikipediaSearch()
-    num_results = 3
-    max_sentences = 5
     
     model = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=config("GOOGLE_API_KEY"))
     chain = {"query": itemgetter("topic")} | wikipedia
