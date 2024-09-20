3c8dd9351e6d64b53130823ac3da1fd0fc34e773
JoaoPito <joao.pito@hotmail.com>
2024-09-20 16:49:02 -0300

    Study Guides: Solved issue #6

--- PencilCase.Web/Pages/StudyGuideGenerator/Models/FragmentDropItem.cs
+++ PencilCase.Web/Pages/StudyGuideGenerator/Models/FragmentDropItem.cs
@@ -5,6 +5,7 @@ namespace PencilCase.Web.Pages.StudyGuideGenerator.Models;
 public class FragmentDropItem
     {
         public String Identifier { get; set; } = String.Empty;
+        public int Order { get; set; }
         public FragmentGenerator Generator { get; set; } = new();
         public Fragment? Fragment { get; set; } = null;
     }
