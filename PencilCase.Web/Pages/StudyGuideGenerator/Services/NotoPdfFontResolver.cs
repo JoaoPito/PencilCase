24ae8b83249a722c7d777a77bb937480b1984a30
JoaoPito <joao.pito@hotmail.com>
2024-10-08 13:28:53 -0300

    Implemented Noto Sans Font Resolver

---
+++ PencilCase.Web/Pages/StudyGuideGenerator/Services/NotoPdfFontResolver.cs
@@ -0,0 +1,35 @@
+using PdfSharp.Drawing;
+using System.IO;
+using PdfSharp.Fonts;
+
+namespace PencilCase.Web.Pages.StudyGuideGenerator.Services;
+
+public class NotoPdfFontResolver : IFontResolver
+{
+    private readonly byte[] _fontData;
+
+    public NotoPdfFontResolver(byte[] fontData)
+    {
+        _fontData = fontData;
+    }
+
+    public byte[]? GetFont(string faceName)
+    {
+        return faceName switch
+        {
+            "NotoSansRegular" => _fontData,
+            _ => null
+        };
+    }
+
+    public FontResolverInfo? ResolveTypeface(string familyName, bool isBold, bool isItalic)
+    {
+        Console.WriteLine($"Family Name: {familyName}");
+        if (familyName.ToLower() == "noto sans")
+        {
+            return new FontResolverInfo("NotoSansRegular");
+        }
+
+        return null;
+    }
+}
