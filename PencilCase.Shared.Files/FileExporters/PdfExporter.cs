f0b1c4ae6b912ecf09428072b24f4a2e3e7118bd
JoaoPito <joao.pito@hotmail.com>
2024-10-08 00:01:18 -0300

    Created PdfExporter class

---
+++ PencilCase.Shared.Files/FileExporters/PdfExporter.cs
@@ -0,0 +1,12 @@
+using PencilCase.Shared.Files.Types;
+using PencilCase.Shared.Models;
+
+namespace PencilCase.Shared.Files.FileExporters;
+
+public class PdfExporter : TextFileExporter
+{
+    public override TextFile ExportStudyGuide(StudyGuide content)
+    {
+        throw new NotImplementedException();
+    }
+}
