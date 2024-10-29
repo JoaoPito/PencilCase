7c20d96ee28058dc60646acc511660a62db08b99
JoaoPito <joao.pito@hotmail.com>
2024-10-28 21:32:49 -0300

    Added API versioning

--- PencilCase.API/Program.cs
+++ PencilCase.API/Program.cs
@@ -1,12 +1,31 @@
+using Asp.Versioning;
 using Microsoft.EntityFrameworkCore;
+using PencilCase.API.Extensions;
 using PencilCase.Shared.Data.Database;
+using PencilCase.Shared.Models.Notebooks;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApiVersioning(options => {
+    options.DefaultApiVersion = new ApiVersion(1);
+    options.ReportApiVersions = true;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new UrlSegmentApiVersionReader(),
+        new HeaderApiVersionReader("X-Api-Version"));
+    })
+    .AddApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'V";
+        options.SubstituteApiVersionInUrl = true;
+    });
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<DAL<Block>>();
 
 var connectionString = builder.Configuration.GetConnectionString("ApiDatabase");
 
@@ -26,6 +45,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 // Blocks endpoints
+app.AddBlocksEndpointsV1();
 
 // BlockProperties endpoints
 
