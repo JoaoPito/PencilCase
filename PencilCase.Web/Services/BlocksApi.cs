5037d066e8818e8905b26dc16331fc462f21875c
JoaoPito <joao.pito@hotmail.com>
2024-11-11 14:57:11 -0300

    Refactored BlocksApi.cs to use BlockViewModel

--- PencilCase.Web/Services/BlocksApi.cs
+++ PencilCase.Web/Services/BlocksApi.cs
@@ -2,45 +2,49 @@ using System.Net.Http.Json;
 using MudBlazor;
 using PencilCase.Shared.DTOs.Requests.Blocks;
 using PencilCase.Shared.DTOs.Responses.Blocks;
+using PencilCase.Web.Pages.Notebooks.Models;
 
 namespace PencilCase.Web.Services;
 
 public class BlocksApi : IBlocksApi
 {
     private readonly HttpClient _httpClient;
-    
-    public BlocksApi(IHttpClientFactory httpClientFactory)
+    private readonly BlockMapper _blockMapper;
+
+    public BlocksApi(IHttpClientFactory httpClientFactory, BlockMapper blockMapper)
     {
         _httpClient = httpClientFactory.CreateClient("BlocksAPI");
+        _blockMapper = blockMapper;
     }
     
-    public async Task<BlockResponse?> GetBlock(Guid id)
+    public async Task<BlockViewModel?> GetBlock(Guid id)
     {
         var blockResponse = await _httpClient.GetFromJsonAsync<BlockResponse>($"{id}");
         if (blockResponse == null)
             throw new NullReferenceException("Response is null!");
-        return blockResponse;
+        return _blockMapper.MapResponseToViewModel(blockResponse);
     }
 
-    public async Task AddBlock(BlockPostRequest block)
+    public async Task AddBlock(BlockViewModel block)
     {
-        var response = await _httpClient.PostAsJsonAsync<BlockPostRequest>($"", block);
+        var request = _blockMapper.MapViewModelToPostRequest(block);
+        var response = await _httpClient.PostAsJsonAsync<BlockPostRequest>($"", request);
         if (!response.IsSuccessStatusCode)
             throw new HttpRequestException($"Error while adding block with name '{block.Name}'");
     }
 
-    public async Task<BlockResponse?> GetLastUsedBlock()
+    public Task<BlockViewModel?> GetLastUsedBlock()
     {
         throw new NotImplementedException();
     }
     
-    public async Task<IEnumerable<BlockResponse>> LoadAllAsync(IEnumerable<Guid> childrenIds)
+    public async Task<IEnumerable<BlockViewModel>> LoadAllAsync(IEnumerable<Guid> blockIds)
     {
-        var children = new List<BlockResponse>();
-        foreach (var id in childrenIds)
+        var children = new List<BlockViewModel>();
+        foreach (var id in blockIds)
         {
-            var child = await GetBlock(id);
-            if (child != null) children = children.Append<BlockResponse>(child).ToList();
+            var block = await GetBlock(id);
+            if (block != null) children = children.Append<BlockViewModel>(block).ToList();
         }
 
         return children;
