4fbbcc845316e4eaea0d76d9c952ca19f1f91f71
JoaoPito <JoaoPito@github.com>
2024-11-23 02:22:54 -0300

    Modified BlockPropertiesResponse.cs to include new field

--- PencilCase.Shared.DTOs/Responses/BlockProperties/BlockPropertiesResponse.cs
+++ PencilCase.Shared.DTOs/Responses/BlockProperties/BlockPropertiesResponse.cs
@@ -1,7 +1,10 @@
+using PencilCase.Shared.Models.Notebooks;
+
 namespace PencilCase.Shared.DTOs.Responses.BlockProperties;
 
 public record class BlockPropertiesResponse(
     int Order, 
     DateTime CreatedOn, 
-    DateTime LastModified
+    DateTime LastModified,
+    CellType CellType
     );
