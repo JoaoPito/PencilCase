aa25c5b3fbbdb8bc8b4531b45a10f94640767eab
JoaoPito <JoaoPito@github.com>
2024-11-23 02:22:20 -0300

    Changed BlockPropertiesPatchRequest.cs and BlockPropertiesRequest.cs to include new field

--- PencilCase.Shared.DTOs/Requests/BlockProperties/BlockPropertiesPatchRequest.cs
+++ PencilCase.Shared.DTOs/Requests/BlockProperties/BlockPropertiesPatchRequest.cs
@@ -1,7 +1,10 @@
+using PencilCase.Shared.Models.Notebooks;
+
 namespace PencilCase.Shared.DTOs.Requests.BlockProperties;
 
 public record class BlockPropertiesPatchRequest
 (
     int? Order,
-    DateTime? LastModified
+    DateTime? LastModified,
+    CellType? CellType
 );
--- PencilCase.Shared.DTOs/Requests/BlockProperties/BlockPropertiesRequest.cs
+++ PencilCase.Shared.DTOs/Requests/BlockProperties/BlockPropertiesRequest.cs
@@ -1,7 +1,10 @@
+using PencilCase.Shared.Models.Notebooks;
+
 namespace PencilCase.Shared.DTOs.Requests.BlockProperties;
 
 public record class BlockPropertiesRequest
 (
     int Order,
-    DateTime LastModified
+    DateTime LastModified,
+    CellType CellType
 );
