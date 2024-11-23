9feda201745bba5307c534ffac336a0e2c7bd124
JoaoPito <JoaoPito@github.com>
2024-11-23 02:22:03 -0300

    Added DB migration

---
+++ PencilCase.Shared.Data/Migrations/20241123052011_AddedCellTypeField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PencilCase.Shared.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCellTypeField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CellType",
+                table: "BlockProperties",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CellType",
+                table: "BlockProperties");
+        }
+    }
+}
