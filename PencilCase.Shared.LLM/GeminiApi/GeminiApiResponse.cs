15eb2b29901dbf581ff9285f70aaf6caca96fb20
JoaoPito <JoaoPito@github.com>
2024-11-24 01:57:54 -0300

    Changed Content field to nullable

--- PencilCase.Shared.LLM/GeminiApi/GeminiApiResponse.cs
+++ PencilCase.Shared.LLM/GeminiApi/GeminiApiResponse.cs
@@ -46,7 +46,7 @@ public class CitationMetadata
 public class Candidate
 {
     [JsonPropertyName("content")]
-    public Content Content { get; set; }
+    public Content? Content { get; set; }
     [JsonPropertyName("finishReason")]
     public string? FinishReason { get; set; }
     [JsonPropertyName("citationMetadata")]
