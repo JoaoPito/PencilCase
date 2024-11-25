ff3d4f82643036e46ad1aa71479ee38763204447
JoaoPito <JoaoPito@github.com>
2024-11-25 20:30:53 -0300

    Created PineconeService.cs

---
+++ PencilCase.LLM.RAG/Providers/Pinecone/PineconeService.cs
@@ -0,0 +1,40 @@
+using System.Reflection.Metadata;
+using Microsoft.Extensions.Configuration;
+using Pinecone;
+
+namespace PencilCase.LLM.RAG.Providers.Pinecone;
+
+public class PineconeService : IRagService
+{
+    private readonly string? _embedModel;
+    private readonly string _defaultNamespace;
+    private readonly PineconeClient _client;
+
+    public PineconeService(IConfiguration configuration)
+    {
+        var apiKey = configuration["Pinecone:ApiKey"];
+        _client = new PineconeClient(apiKey);
+        _embedModel = configuration["Pinecone:EmbedModel"] ?? "multilingual-e5-large";
+        _defaultNamespace = configuration["Pinecone:DefaultNamespace"] ?? "pencilcase";
+    }
+    
+    public List<Document> GetDocsByQuery(string query, List<Guid> parentIds)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddDocs(List<Document> docs)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteDocs(List<Guid> docIds)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateDocs(List<Document> docs)
+    {
+        throw new NotImplementedException();
+    }
+}
