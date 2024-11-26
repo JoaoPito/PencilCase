cc8a3dce9a274087ba28eb7c618640ac4ff51cbf
JoaoPito <JoaoPito@github.com>
2024-11-26 01:47:50 -0300

    Changed UpdateDocs to UpdateDoc on IRagService.cs

--- PencilCase.LLM.RAG/Providers/IRagService.cs
+++ PencilCase.LLM.RAG/Providers/IRagService.cs
@@ -7,5 +7,5 @@ public interface IRagService
      public Task<List<Document>?> GetDocsByQuery(String query, List<Guid> parentIds, uint nResults = 3);
      public Task AddDocs(List<Document> docs);
      public Task DeleteSingleDoc(Guid id);
-     public Task UpdateDocs(List<Document> docs);
+     public Task UpdateDoc(Document doc);
 }
