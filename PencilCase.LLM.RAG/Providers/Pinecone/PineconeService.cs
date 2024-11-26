97b14c4b1fbe23da38f9ef45f20be9ef679e167f
JoaoPito <JoaoPito@github.com>
2024-11-25 23:23:55 -0300

    Implemented method AddDocs on PineconeService.cs

--- PencilCase.LLM.RAG/Providers/Pinecone/PineconeService.cs
+++ PencilCase.LLM.RAG/Providers/Pinecone/PineconeService.cs
@@ -7,28 +7,56 @@ namespace PencilCase.LLM.RAG.Providers.Pinecone;
 
 public class PineconeService : IRagService
 {
-    private readonly string? _embedModel;
+    private readonly string _embedModel;
     private readonly string _defaultNamespace;
     private readonly PineconeClient _client;
+    private readonly string _defaultIndex;
 
     public PineconeService(IConfiguration configuration)
     {
         var apiKey = configuration["Pinecone:ApiKey"];
         _client = new PineconeClient(apiKey);
         _embedModel = configuration["Pinecone:EmbedModel"] ?? "multilingual-e5-large";
-        _defaultNamespace = configuration["Pinecone:DefaultNamespace"] ?? "pencilcase";
+        _defaultIndex = configuration["Pinecone:DefaultIndex"] ?? "pencilcase";
+        _defaultNamespace = configuration["Pinecone:DefaultNamespace"] ?? "";
     }
     
-    public List<Document> GetDocsByQuery(string query, List<Guid> parentIds)
+    public List<Document> GetDocsByQuery(string query, List<Guid> parentIds, int nResults = 3)
     {
         throw new NotImplementedException();
     }
 
-    public Task AddDocs(List<Document> docs)
+    public async Task AddDocs(List<Document> docs)
     {
-        var data = docs.Select(i => new EmbedRequestInputsItem()
+        var embeddings = await _client.Inference.EmbedAsync(new EmbedRequest()
         {
-            Text = i.Content,
+            Model = _embedModel,
+            Inputs = docs.Select(i => new EmbedRequestInputsItem() { Text = i.Content }),
+            Parameters = new EmbedRequestParameters()
+            {
+                InputType = "passage",
+                Truncate = "END"
+            }
+        });
+
+        var embeddingsData = embeddings.Data;
+
+        var index = _client.Index(_defaultIndex);
+
+        List<Vector> records = embeddingsData.Select((e, idx) => new Vector()
+        {
+            Id = docs[idx].Id.ToString(),
+            Values = new ReadOnlyMemory<float>(e.Values?.Select(Convert.ToSingle).ToArray()),
+            Metadata = new Metadata()
+            {
+                ["parent_id"] = docs[idx].ParentId.ToString()
+            }
+        }).ToList();
+
+        await index.UpsertAsync(new UpsertRequest()
+        {
+            Vectors = records,
+            Namespace = _defaultNamespace
         });
     }
 
