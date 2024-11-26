9d86b1a108b977c30d5cba3789af879ee81a81c5
JoaoPito <JoaoPito@github.com>
2024-11-25 23:38:55 -0300

    Added n_results to QueryRequest.cs

--- PencilCase.LLM.RAG/Requests/QueryRequest.cs
+++ PencilCase.LLM.RAG/Requests/QueryRequest.cs
@@ -4,4 +4,5 @@ public class QueryRequest
 {
     public String Query { get; set; } = String.Empty;
     public List<Guid> ParentIds { get; set; } = new();
+    public int? NResults { get; set; } = 3;
 }
