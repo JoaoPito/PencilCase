fcf468f7d42efe346405f32717b2bf90366d36a6
JoaoPito <JoaoPito@github.com>
2024-11-25 23:37:58 -0300

    Updated method GetDocsByQuery to support async on IRagService.cs

--- PencilCase.LLM.RAG/Providers/IRagService.cs
+++ PencilCase.LLM.RAG/Providers/IRagService.cs
@@ -4,7 +4,7 @@ namespace PencilCase.LLM.RAG.Providers;
 
 public interface IRagService
 {
-     public List<Document> GetDocsByQuery(String query, List<Guid> parentIds, int nResults = 3);
+     public Task<List<Document>> GetDocsByQuery(String query, List<Guid> parentIds, int nResults = 3);
      public Task AddDocs(List<Document> docs);
      public Task DeleteDocs(List<Guid> docIds);
      public Task UpdateDocs(List<Document> docs);
