d5d524523c2eae9d227b033bf35b9cf791938905
JoaoPito <JoaoPito@github.com>
2024-11-27 09:26:56 -0300

    Implemented TelemetryDbContext DAL class

---
+++ PencilCase.Telemetry.Data/Database/DAL.cs
@@ -0,0 +1,45 @@
+namespace PencilCase.Telemetry.Data.Database;
+
+public class DAL<T> where T: class
+{
+    private readonly TelemetryDbContext _context;
+    
+    public DAL(TelemetryDbContext context)
+    {
+        this._context = context;
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        return _context.Set<T>().ToList();
+    }
+
+    public IEnumerable<T> GetAllBy(Func<T, bool> criteria)
+    {
+        return _context.Set<T>().Where(criteria).ToList();
+    }
+
+    public T? GetBy(Func<T, bool> criteria)
+    {
+        return _context.Set<T>().FirstOrDefault(criteria);
+    }
+
+    public async Task Add(T item)
+    {
+        _context.Add(item);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Update(T item)
+    {
+        _context.Update(item);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Delete(T item)
+    {
+        _context.Remove(item);
+        await _context.SaveChangesAsync();
+    }
+
+}
