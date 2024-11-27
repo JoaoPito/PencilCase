90043d6cdc2b84e1ff37374f86de0d0ff0800047
JoaoPito <JoaoPito@github.com>
2024-11-27 13:06:21 -0300

    Added RagOperationEntries-Create Migration

---
+++ PencilCase.Telemetry.Data/Migrations/20241127160520_RagOperationEntries-Create.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PencilCase.Telemetry.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RagOperationEntriesCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RagOperationEntries",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    OperationType = table.Column<int>(type: "integer", nullable: false),
+                    TokensUsed = table.Column<long>(type: "bigint", nullable: false),
+                    UnitsUsed = table.Column<long>(type: "bigint", nullable: false),
+                    FileSizeBytes = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RagOperationEntries", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RagOperationEntries");
+        }
+    }
+}
