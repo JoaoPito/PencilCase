8b48d7663162d7b89ccebac973fe2d5be7ed676c
JoaoPito <JoaoPito@github.com>
2024-11-27 17:54:19 -0300

    FIxed ILlmApi.cs query method signature

--- PencilCase.Web/Services/LLM/ILlmApi.cs
+++ PencilCase.Web/Services/LLM/ILlmApi.cs
@@ -7,6 +7,6 @@ public interface ILlmApi
 {
     public Task<IEnumerable<LlmMessage>> InvokeLlmAgentAsync(IEnumerable<LlmMessage> llmChat);
     public Task AddRagDocumentsAsync(IEnumerable<RagDocument> documents);
-    public Task<IEnumerable<RagDocument>> QueryRagDocumentsAsync(string llmId);
+    public Task<IEnumerable<RagDocument>> QueryRagDocumentsAsync(string query, List<Guid> parentIds, uint? nResults=3);
     public Task DeleteRagDocumentsAsync(IEnumerable<RagDocument> documents);
 }
