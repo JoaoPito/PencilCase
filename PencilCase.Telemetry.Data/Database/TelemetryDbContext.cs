31376e6bf8038d219121d1abb9344f649af4f594
JoaoPito <JoaoPito@github.com>
2024-11-27 13:05:53 -0300

    Added RagOperationEntries DbSet To TelemetryDbContext.cs

--- PencilCase.Telemetry.Data/Database/TelemetryDbContext.cs
+++ PencilCase.Telemetry.Data/Database/TelemetryDbContext.cs
@@ -1,11 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using PencilCase.Shared.Models.Telemetry.LLM.Agents;
+using PencilCase.Shared.Models.Telemetry.LLM.RAG;
 
 namespace PencilCase.Telemetry.Data.Database;
 
 public class TelemetryDbContext : DbContext
 {
     public virtual DbSet<GenerationResultEntry> GenerationResultEntries { get; set; }
+    public virtual DbSet<RagOperationEntry> RagOperationEntries { get; set; }
 
     public TelemetryDbContext(DbContextOptions options) : base(options)
     {
