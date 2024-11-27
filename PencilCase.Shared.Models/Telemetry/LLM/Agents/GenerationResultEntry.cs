13af663f3b9b94944b0177259985396e34069cae
JoaoPito <JoaoPito@github.com>
2024-11-27 14:20:52 -0300

    Added Model field to GenerationResultEntry.cs

--- PencilCase.Shared.Models/Telemetry/LLM/Agents/GenerationResultEntry.cs
+++ PencilCase.Shared.Models/Telemetry/LLM/Agents/GenerationResultEntry.cs
@@ -7,5 +7,6 @@ public record GenerationResultEntry()
     public int PromptTokenCount { get; set; }
     public int GenerationTokenCount { get; set; }
     public int GenerationCharCount { get; set; }
+    public string Model { get; set; } = String.Empty;
     public string? FinishReason { get; set; }
 }
