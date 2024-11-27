843492a105f30214baf6b54be7cb89ef8cc5340f
JoaoPito <JoaoPito@github.com>
2024-11-27 14:18:54 -0300

    Added Agents-AddedModelColumn db migration

---
+++ PencilCase.Telemetry.Data/Migrations/20241127171749_Agents-AddedModelColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PencilCase.Telemetry.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgentsAddedModelColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Model",
+                table: "GenerationResultEntries",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Model",
+                table: "GenerationResultEntries");
+        }
+    }
+}
