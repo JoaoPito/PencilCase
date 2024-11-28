bc7cf4c7eccb237f3d56ed6204bb2fa97893a2bd
JoaoPito <JoaoPito@github.com>
2024-11-27 22:06:38 -0300

    Using LLM Api on CellView.razor.cs

--- PencilCase.Web/Pages/Notebooks/Notebooks/CellView.razor.cs
+++ PencilCase.Web/Pages/Notebooks/Notebooks/CellView.razor.cs
@@ -2,8 +2,12 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using MudBlazor;
 using MudBlazor.Extensions;
+using PencilCase.LLM.Agents.Providers.Gemini;
+using PencilCase.Shared.Models.LLM.Agents;
+using PencilCase.Shared.Models.LLM.RAG;
 using PencilCase.Shared.Models.Notebooks;
 using PencilCase.Web.Pages.Notebooks.Models;
+using PencilCase.Web.Services.LLM;
 using PencilCase.Web.Services.Notebooks;
 
 namespace PencilCase.Web.Pages.Notebooks.Notebooks;
@@ -16,6 +20,7 @@ public partial class CellView : ComponentBase
     [Parameter] public BlockViewModel? Block { get; set; }
     [Parameter] public IBlocksApi BlocksApi { get; set; } = null!;
     [Parameter] public EventCallback? OnNewCellShortcut { get; set; }
+    [Inject] public ILlmApi LlmApi { get; set; }
 
     IEnumerable<BlockViewModel> _loadedChildren = new List<BlockViewModel>();
     BlockViewModel? _shownChild;
@@ -105,9 +110,10 @@ public partial class CellView : ComponentBase
     }
     
     int _exampleCounter = 0;
+
     async Task GenerateOutputIfPossible()
     {
-        if (IsGenerator())
+        if (IsGenerator() && !string.IsNullOrWhiteSpace(Block!.Name))
         {
             _isLoading = true;
             _childError = false;
@@ -115,34 +121,92 @@ public partial class CellView : ComponentBase
 
             try
             {
-                // Example generation for testing
-                var exampleGeneration = new BlockViewModel()
-                {
-                    Id = new Guid(),
-                    Name = $"{_exampleCounter++} - ASCII, short for American Standard Code for Information Interchange, is a character encoding standard used to represent text in computers and other devices that handle text.\nIt was developed in the 1960s and became widely adopted as a standard for communication between different systems.",
-                    ParentId = Guid.Parse("808d779b-c6a5-4a8b-aa4d-ea1b4a7f6ae1"),
-                    Properties = new BlockPropertiesViewModel()
-                    {
-                        CellType = CellType.Text,
-                        CreatedOn = DateTime.UtcNow,
-                        LastModified = DateTime.UtcNow,
-                        Order = _shownChild is null ? 0 : _shownChild.Properties.Order + 1
-                    }
-                };
-                Block!.ChildrenIds = Block!.ChildrenIds.Append(exampleGeneration.Id);
-                _loadedChildren = _loadedChildren.Append(exampleGeneration);
-                await Task.Delay(2000);
+                var genResult = await GenerateAnswersTo(Block!.Name);
                 
-                SwapShownChildAndUpdate(exampleGeneration);
+                foreach (var block in genResult)
+                {
+                    Block!.ChildrenIds = Block!.ChildrenIds.Append(block.Id);
+                    _loadedChildren = _loadedChildren.Append(block);
+                
+                    SwapShownChildAndUpdate(block);
+                }
             }
             catch (Exception)
             {
                 _childError = true;
+                throw;
             }
             _isLoading = false;
         }
     }
 
+    async Task<List<BlockViewModel>> GenerateAnswersTo(string query)
+    {
+        if (Block!.ParentId is null)
+            throw new ArgumentException("Cannot generate LLM result on root block!");
+
+        var parentBlock = await BlocksApi.GetBlock(Block!.ParentId ?? new Guid());
+        
+        if (parentBlock!.ParentId is null)
+            throw new ArgumentException("Cannot generate LLM result on root block!");
+        
+        var parentIds = new List<Guid> { parentBlock!.ParentId ?? new Guid() };
+        Console.WriteLine($"Sending query to RAG with query {query} and parentIds {parentIds.First()}");
+        
+        var ragResults = await LlmApi.QueryRagDocumentsAsync(query,parentIds);
+        Console.WriteLine($"Got {ragResults.Count()} results from RAG. ");
+        
+        var llmResults = await LlmApi.InvokeLlmAgentAsync(
+            BuildChatMessagesWithRag(Block!.Name, ragResults.ToList()));
+        
+        return llmResults.Select(r => new BlockViewModel()
+        {
+            Id = new Guid(),
+            Name = r.Content,
+            ParentId = Block!.Id,
+            Type = BlockType.Cell,
+            Properties = new BlockPropertiesViewModel()
+            {
+                CellType = CellType.Text,
+                CreatedOn = DateTime.UtcNow,
+                LastModified = DateTime.UtcNow,
+                Order = _shownChild is null ? 0 : _shownChild.Properties.Order + 1
+            }
+        }).ToList();
+    }
+
+    List<LlmMessage> BuildChatMessagesWithRag(string prompt, List<RagDocument>? docs)
+    {
+        var chatMessages = new List<LlmMessage>();
+        
+        // Append messages for other blocks in notebook
+        
+        if (docs is not null)
+        {
+            string docsMessage = "";
+        
+            for(int i = 0; i < docs!.Count(); i++)
+            {
+                if(!string.IsNullOrWhiteSpace(docs[i].Content))
+                {
+                    docsMessage += $"# CHUNK {i+1}\n{docs[i].Content}\n";
+                    Console.WriteLine($"docsMessage: {docsMessage}");
+                }
+            }
+            if(!string.IsNullOrWhiteSpace(docsMessage))
+                chatMessages = chatMessages.Append(new LlmMessage()
+                {
+                    Role = "user",
+                    Content = docsMessage,
+                }).ToList();
+        }
+        return chatMessages.Append(new LlmMessage()
+        {
+            Role = "user",
+            Content = prompt,
+        }).ToList();
+    }
+    
     bool IsGenerator()
     {
         return Block?.Properties.CellType is CellType.Question;
