67e6527a5a02a9c9f87d28875e5098565f3c89ad
JoaoPito <JoaoPito@github.com>
2024-11-30 01:34:29 -0300

    Changed GetDocsByQuery method name to GetChunksForQuery

--- PencilCase.LLM/RAG/IRagService.cs
+++ PencilCase.LLM/RAG/IRagService.cs
@@ -1,10 +1,11 @@
 using PencilCase.Shared.Models.LLM.RAG;
+using PencilCase.Shared.Models.Notebooks;
 
 namespace PencilCase.LLM.RAG;
 
 public interface IRagService
 {
-     public Task<List<RagDocument>?> GetDocsByQuery(String query, List<Guid> parentIds, uint nResults = 3);
+     public Task<List<RagDocument>?> GetChunksForQuery(String query, List<Guid> filterIds, uint nResults = 3);
      public Task AddDocs(List<RagDocument> docs);
      public Task DeleteSingleDoc(RagDocument doc);
      public Task UpdateDoc(RagDocument doc);
