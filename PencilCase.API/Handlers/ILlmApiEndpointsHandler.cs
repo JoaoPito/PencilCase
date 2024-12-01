bbf62b90f9673cb6e4ae5363a7c819bc6dacea02
JoaoPito <JoaoPito@github.com>
2024-12-01 17:37:37 -0300

    Methods in ILlmApiEndpointsHandler.cs now return IResults

--- PencilCase.API/Handlers/ILlmApiEndpointsHandler.cs
+++ PencilCase.API/Handlers/ILlmApiEndpointsHandler.cs
@@ -6,8 +6,8 @@ namespace PencilCase.API.Handlers;
 
 public interface ILlmApiEndpointsHandler
 {
-    public Task AddChunksAsync(IEnumerable<Block> chunks);
-    public Task<List<Block>> SearchForChunksAsync(Block query);
-    public Task<List<LlmMessage>> InvokeAgentAsync(List<LlmMessage> chat, List<Block>? docs);
-    public Task DeleteChunksAsync(List<Block> chunks);
+    public Task<IResult> AddChunksAsync(IEnumerable<Block> chunks);
+    public Task<IResult> SearchForChunksAsync(Block query);
+    public Task<IResult> InvokeAgentAsync(List<LlmMessage> chat, List<Block>? docs);
+    public Task<IResult> DeleteChunksAsync(List<Block> chunks);
 }
