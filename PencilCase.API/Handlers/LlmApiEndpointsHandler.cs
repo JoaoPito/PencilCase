1d2d83a4ba566201c4ec883a3a5fdd48914db1a4
JoaoPito <JoaoPito@github.com>
2024-12-01 16:35:28 -0300

    Implemented method signatures on LlmApiEndpointsHandler.cs

--- PencilCase.API/Handlers/LlmApiEndpointsHandler.cs
+++ PencilCase.API/Handlers/LlmApiEndpointsHandler.cs
@@ -1,5 +1,7 @@
 using PencilCase.LLM.Agents.Providers;
 using PencilCase.LLM.RAG;
+using PencilCase.Shared.Models.LLM.Agents;
+using PencilCase.Shared.Models.Notebooks;
 
 namespace PencilCase.API.Handlers;
 
@@ -8,5 +10,25 @@ public class LlmApiEndpointsHandler : ILlmApiEndpointsHandler
     public LlmApiEndpointsHandler(IRagService ragService, ILlmApiService llmApiService)
     {
         
+    }
+
+    public Task AddChunksAsync(IEnumerable<Block> chunks)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Block>> SearchForChunksAsync(Block query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<LlmMessage>> InvokeAgentAsync(List<LlmMessage> chat, List<Block>? docs)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteChunksAsync(List<Block> chunks)
+    {
+        throw new NotImplementedException();
     }
 }
