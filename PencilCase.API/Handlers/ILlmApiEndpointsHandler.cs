ceee513b8796b7915d92f6812904b9034c447312
JoaoPito <JoaoPito@github.com>
2024-12-01 16:31:20 -0300

    Implemented interface ILlmApiEndpointsHandler.cs

--- PencilCase.API/Handlers/ILlmApiEndpointsHandler.cs
+++ PencilCase.API/Handlers/ILlmApiEndpointsHandler.cs
@@ -1,6 +1,13 @@
+using PencilCase.Shared.Models.LLM.Agents;
+using PencilCase.Shared.Models.LLM.RAG;
+using PencilCase.Shared.Models.Notebooks;
+
 namespace PencilCase.API.Handlers;
 
 public interface ILlmApiEndpointsHandler
 {
-    
+    public Task AddChunksAsync(IEnumerable<Block> chunks);
+    public Task<List<Block>> SearchForChunksAsync(Block query);
+    public Task<List<LlmMessage>> InvokeAgentAsync(List<LlmMessage> chat, List<Block>? docs);
+    public Task DeleteChunksAsync(List<Block> chunks);
 }
