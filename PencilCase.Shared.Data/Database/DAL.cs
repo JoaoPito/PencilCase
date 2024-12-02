1403d87145e644378e679aba2fa1c223f31041e2
JoaoPito <JoaoPito@github.com>
2024-12-01 22:30:55 -0300

    Changed DAL.cs parameter to protected

--- PencilCase.Shared.Data/Database/DAL.cs
+++ PencilCase.Shared.Data/Database/DAL.cs
@@ -5,8 +5,9 @@ namespace PencilCase.Shared.Data.Database;
 
 public class DAL<T> where T: class
 {
-    private readonly BlocksDbContext _context;
-    public DAL(BlocksDbContext context)
+    protected readonly DbContext _context;
+
+    protected DAL(DbContext context)
     {
         this._context = context;
     }
