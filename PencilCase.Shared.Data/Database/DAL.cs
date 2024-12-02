f1196064117e749a398d4f8e8d08525540db49e1
JoaoPito <JoaoPito@github.com>
2024-12-01 22:31:43 -0300

    Changed DAL.cs context variable name from "_context" to "Context"

--- PencilCase.Shared.Data/Database/DAL.cs
+++ PencilCase.Shared.Data/Database/DAL.cs
@@ -5,43 +5,43 @@ namespace PencilCase.Shared.Data.Database;
 
 public class DAL<T> where T: class
 {
-    protected readonly DbContext _context;
+    protected readonly DbContext Context;
 
     protected DAL(DbContext context)
     {
-        this._context = context;
+        this.Context = context;
     }
 
     public IEnumerable<T> GetAll()
     {
-        return _context.Set<T>().ToList();
+        return Context.Set<T>().ToList();
     }
 
     public IEnumerable<T> GetAllBy(Func<T, bool> criteria)
     {
-        return _context.Set<T>().Where(criteria).ToList();
+        return Context.Set<T>().Where(criteria).ToList();
     }
 
     public T? GetBy(Func<T, bool> criteria)
     {
-        return _context.Set<T>().FirstOrDefault(criteria);
+        return Context.Set<T>().FirstOrDefault(criteria);
     }
 
     public async Task Add(T item)
     {
-        _context.Add(item);
-        await _context.SaveChangesAsync();
+        Context.Add(item);
+        await Context.SaveChangesAsync();
     }
 
     public async Task Update(T item)
     {
-        _context.Update(item);
-        await _context.SaveChangesAsync();
+        Context.Update(item);
+        await Context.SaveChangesAsync();
     }
 
     public async Task Delete(T item)
     {
-        _context.Remove(item);
-        await _context.SaveChangesAsync();
+        Context.Remove(item);
+        await Context.SaveChangesAsync();
     }
 }
