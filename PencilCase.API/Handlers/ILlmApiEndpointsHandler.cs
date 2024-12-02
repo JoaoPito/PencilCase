721840cae69265d6c57cd4f464e650f908c0445f
JoaoPito <JoaoPito@github.com>
2024-12-02 00:59:00 -0300

    Reverted mistake of making SearchForChunks methos synchronous

--- PencilCase.API/Handlers/ILlmApiEndpointsHandler.cs
+++ PencilCase.API/Handlers/ILlmApiEndpointsHandler.cs
@@ -7,7 +7,7 @@ namespace PencilCase.API.Handlers;
 public interface ILlmApiEndpointsHandler
 {
     public Task<IResult> AddChunksAsync(IEnumerable<Block> chunks);
-    public IResult SearchForChunks(Block query);
+    public Task<IResult> SearchForChunksAsync(Block query);
     public Task<IResult> InvokeAgentAsync(List<LlmMessage> chat);
     public Task<IResult> DeleteChunksAsync(List<Block> chunks);
 }
