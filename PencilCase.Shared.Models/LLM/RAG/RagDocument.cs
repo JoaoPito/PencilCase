e2f443372cca1050fc8003e748d1c7f843bd43bf
JoaoPito <JoaoPito@github.com>
2024-12-02 13:49:55 -0300

    Implemented equality members on RagDocument.cs

--- PencilCase.Shared.Models/LLM/RAG/RagDocument.cs
+++ PencilCase.Shared.Models/LLM/RAG/RagDocument.cs
@@ -5,4 +5,25 @@ public class RagDocument
     public Guid Id { get; set; } = new Guid();
     public Guid ParentId { get; set; } = new Guid();
     public String Content { get; set; } = string.Empty;
+
+    public override string ToString()
+    {
+        return $"Id: {Id.ToString()}, Content: \"{String.Concat(Content.Take(50))}\"";
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is not RagDocument) return false;
+        return this.Equals((RagDocument)obj);
+    }
+
+    protected bool Equals(RagDocument other)
+    {
+        return Id.Equals(other.Id) && ParentId.Equals(other.ParentId) && Content == other.Content;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, ParentId, Content);
+    }
 }
