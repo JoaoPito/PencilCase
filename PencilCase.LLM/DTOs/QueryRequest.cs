d26f1f8e8535900cca165b43a2d2fdcf581f5431
JoaoPito <JoaoPito@github.com>
2024-12-04 02:05:38 -0300

    Removed RAG QueryRequest DTO

--- PencilCase.LLM/DTOs/QueryRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace PencilCase.LLM.DTOs;
-
-public class QueryRequest
-{
-    public String Query { get; set; } = String.Empty;
-    public List<Guid> ParentIds { get; set; } = new();
-    public uint? NResults { get; set; } = 3;
-}
