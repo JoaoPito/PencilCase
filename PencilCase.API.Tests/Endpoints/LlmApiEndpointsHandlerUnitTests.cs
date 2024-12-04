db5a8bdb5d60ce0bd2be627b93ce0d6d3bd74b4f
JoaoPito <JoaoPito@github.com>
2024-12-04 00:37:53 -0300

    Implemented InvokeAgentAsync_ReturnsOkResponse_IfSuccessful unit test

--- PencilCase.API.Tests/Endpoints/LlmApiEndpointsHandlerUnitTests.cs
+++ PencilCase.API.Tests/Endpoints/LlmApiEndpointsHandlerUnitTests.cs
@@ -337,6 +337,36 @@ public class LlmApiEndpointsHandlerUnitTests
     }
 
     [Test]
+    public async Task InvokeAgentAsync_ReturnsOkResponse_IfSuccessful()
+    {
+        var query = new LlmMessage()
+        {
+            Role = "user",
+            Content = "This is a query from a hopefully human user"
+        };
+        
+        var generatedContent = new LlmMessage()
+        {
+            Role = "model",
+            Content = "This is a response from the wonderful LLM of your choice."
+        };
+        
+        _llmApiServiceMock.Setup(s => s.GenerateContent(
+                It.IsAny<List<LlmMessage>>(),
+                It.IsAny<LlmMessage?>()))
+            .ReturnsAsync(new List<LlmMessage>()
+            {
+                generatedContent
+            });
+        
+        var handler = new LlmApiEndpointsHandler(_ragServiceMock.Object, _llmApiServiceMock.Object, _blocksDalMock.Object);
+
+        var response = await handler.InvokeAgentAsync(new List<LlmMessage>() { query });
+        
+        Assert.That(response, Is.TypeOf<Ok<List<LlmMessage>>>());
+    }
+
+    [Test]
     public async Task InvokeAgentAsync_ReturnsCorrectLlmMessageContents()
     {
         var query = new LlmMessage()
