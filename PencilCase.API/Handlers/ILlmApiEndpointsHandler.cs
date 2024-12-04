865b46732ece41a257380fcf9344e2c8227afe43
JoaoPito <JoaoPito@github.com>
2024-12-04 01:19:39 -0300

    ILlmApiEndpointsHandler.cs now uses DTO for searching

--- PencilCase.API/Handlers/ILlmApiEndpointsHandler.cs
+++ PencilCase.API/Handlers/ILlmApiEndpointsHandler.cs
@@ -8,7 +8,7 @@ namespace PencilCase.API.Handlers;
 public interface ILlmApiEndpointsHandler
 {
     public Task<IResult> AddChunksAsync(IEnumerable<RagDocumentAddRequest> chunks);
-    public Task<IResult> SearchForChunksAsync(Block query);
+    public Task<IResult> SearchForChunksAsync(RagDocumentSearchRequest query);
     public Task<IResult> InvokeAgentAsync(List<LlmMessage> chat);
     public Task<IResult> DeleteChunksAsync(List<Block> chunks);
 }
