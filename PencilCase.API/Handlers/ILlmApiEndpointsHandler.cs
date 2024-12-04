98d7acb858539c9a247edf692c9032e997dd85ee
JoaoPito <JoaoPito@github.com>
2024-12-04 01:30:07 -0300

    Updated ILlmApiEndpointsHandler.cs to use new DTO

--- PencilCase.API/Handlers/ILlmApiEndpointsHandler.cs
+++ PencilCase.API/Handlers/ILlmApiEndpointsHandler.cs
@@ -1,6 +1,5 @@
+using PencilCase.Shared.DTOs.Requests.Llm;
 using PencilCase.Shared.DTOs.Requests.Rag;
-using PencilCase.Shared.Models.LLM.Agents;
-using PencilCase.Shared.Models.LLM.RAG;
 using PencilCase.Shared.Models.Notebooks;
 
 namespace PencilCase.API.Handlers;
@@ -9,6 +8,6 @@ public interface ILlmApiEndpointsHandler
 {
     public Task<IResult> AddChunksAsync(IEnumerable<RagDocumentAddRequest> chunks);
     public Task<IResult> SearchForChunksAsync(RagDocumentSearchRequest query);
-    public Task<IResult> InvokeAgentAsync(List<LlmMessage> chat);
+    public Task<IResult> InvokeAgentAsync(LlmMessageInvokeRequest chatMessages);
     public Task<IResult> DeleteChunksAsync(List<Block> chunks);
 }
