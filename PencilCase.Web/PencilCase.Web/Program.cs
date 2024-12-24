35a87aa97c0889cb5ce16fa75e2e699e2cb349b8
JoaoPito <JoaoPito@github.com>
2024-12-24 13:07:04 -0300

    Added MudBlazor dependency

--- PencilCase.Web/PencilCase.Web/Program.cs
+++ PencilCase.Web/PencilCase.Web/Program.cs
@@ -1,3 +1,4 @@
+using MudBlazor.Services;
 using PencilCase.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
