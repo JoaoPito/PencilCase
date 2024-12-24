27d5a098698d1d5976d28cc23a12352c712eba1f
JoaoPito <JoaoPito@github.com>
2024-12-24 12:54:05 -0300

    Removed PencilCase.Web project files

--- PencilCase.Web/Pages/StudyGuideGenerator/Models/StudyGuideToolbarActions.cs
+++
@@ -1,9 +0,0 @@
-namespace PencilCase.Web.Pages.StudyGuideGenerator.Models;
-
-public record DraftToolbarActions
-{
-    public Action<String>? OnRegenerate { get; set; } 
-    public Action<String>? OnDelete { get; set; } 
-    public Action<String>? OnShare { get; set; } 
-
-}
