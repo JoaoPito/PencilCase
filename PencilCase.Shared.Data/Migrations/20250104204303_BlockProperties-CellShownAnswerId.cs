d3de8f05d8b32d39e4ca381348b5e03f19a6991f
JoaoPito <JoaoPito@github.com>
2025-01-04 17:43:41 -0300

    Added DB migrations

---
+++ PencilCase.Shared.Data/Migrations/20250104204303_BlockProperties-CellShownAnswerId.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PencilCase.Shared.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class BlockPropertiesCellShownAnswerId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CellShownAnswerId",
+                table: "BlockProperties",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CellShownAnswerId",
+                table: "BlockProperties");
+        }
+    }
+}
