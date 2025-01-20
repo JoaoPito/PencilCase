b0eb5dfe4efda12a87dfc6683eced956f97d92e8
JoaoPito <JoaoPito@github.com>
2025-01-20 17:18:41 -0300

    Created SourcesWorker.cs class

---
+++ PencilCase.API/Workers/SourcesWorker.cs
@@ -0,0 +1,9 @@
+namespace PencilCase.LLM.Workers;
+
+public class SourcesWorker: BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+} 
