71d6c5f975dcf89119292177b29bf13d6a5b5174
JoaoPito <JoaoPito@github.com>
2025-01-21 17:56:43 -0300

    Renamed SourceBlockId to ParentBlockId

--- PencilCase.Shared.Models/LLM/Parser/ParserJob.cs
+++ PencilCase.Shared.Models/LLM/Parser/ParserJob.cs
@@ -14,5 +14,5 @@ public class ParserJob
     public JobStatus Status { get; set; }
     public string? StatusMsg { get; set; }
     public ParserFile File { get; set; } = new();
-    public Guid SourceBlockId { get; set; }
+    public Guid ParentBlockId { get; set; }
 }
