4ad30cd925b5d69f10dd4184aba974b93262d1f1
JoaoPito <JoaoPito@github.com>
2025-01-21 14:30:09 -0300

    Added Json property names for ParserFile.cs attributes

--- PencilCase.Shared.Models/LLM/Parser/ParserFile.cs
+++ PencilCase.Shared.Models/LLM/Parser/ParserFile.cs
@@ -1,7 +1,11 @@
+using System.Text.Json.Serialization;
+
 namespace PencilCase.Shared.Models.LLM.Parser;
 
 public class ParserFile
 {
+    [JsonPropertyName("filename")]
     public string Name { get; set; } = string.Empty;
+    [JsonPropertyName("file_contents")]
     public string Contents { get; set; } = string.Empty;
 }
