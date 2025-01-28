f62eebfc384452b6bffe2c0f411689a8751de3c2
JoaoPito <JoaoPito@github.com>
2025-01-28 14:48:57 -0300

    First SourceUploadDialog.razor and SourceUploadDialog.razor.cs implementation

---
+++ PencilCase.Web/Pages/Notebooks/Upload/SourceUploadDialog.razor.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Web;
+using MudBlazor;
+using PencilCase.Shared.DTOs.Requests.Sources;
+
+namespace PencilCase.Web.Pages.Notebooks.Upload;
+
+public partial class SourceUploadDialog : ComponentBase
+{
+    [Parameter] public Guid ParentId { get; set; } = Guid.Empty;
+    
+    private MudFileUpload<IReadOnlyList<IBrowserFile>>? _fileUpload;
+    
+    private IBrowserFile? _file;
+    private string _errorMsg = "";
+    private bool _showError = false;
+    
+    private async Task UploadFileAsync(MouseEventArgs arg)
+    {
+        // Start upload job
+        // While job is not completed
+            // At each N seconds, get the job status and show to the user
+        throw new NotImplementedException();
+    }
+    
+    private async Task<UploadSourceRequest> ConvertFileToUploadRequest(IBrowserFile file)
+    {
+        return new UploadSourceRequest()
+        {
+            ParentBlockId = ParentId,
+            FileName = file.Name,
+            FileContents = await ConvertFileContentsToBase64(file)
+        };
+    }
+    
+    private async Task<string> ConvertFileContentsToBase64(IBrowserFile file)
+    {
+        using var ms = new MemoryStream();
+        await file.OpenReadStream().CopyToAsync(ms);
+        return Convert.ToBase64String(ms.ToArray());
+    }
+    
+    private async Task ClearAsync()
+    {
+        await (_fileUpload?.ClearAsync() ?? Task.CompletedTask);
+        _file = null;
+        _showError = false;
+        _errorMsg = "";
+        ClearDragClass();
+    }
+}
