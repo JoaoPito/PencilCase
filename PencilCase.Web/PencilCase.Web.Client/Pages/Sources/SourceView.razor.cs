5ca29b5e8da29bc0acb87b8cf46b5ad9c7a6b564
JoaoPito <JoaoPito@github.com>
2025-01-31 19:10:35 -0300

    Added SourceView.razor and SourceView.razor.cs

---
+++ PencilCase.Web/PencilCase.Web.Client/Pages/Sources/SourceView.razor.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Components;
+using PencilCase.Web.Client.Services.Blocks;
+using PencilCase.Web.Client.ViewModels;
+
+namespace PencilCase.Web.Client.Pages.Sources;
+
+public partial class SourceView : ComponentBase
+{
+    [CascadingParameter(Name = "block")] protected BlockViewModel? Block { get; set; }
+    [Parameter] public IBlocksApi BlocksApi { get; set; } = null!;
+    private List<BlockViewModel> _children = [];
+    private bool _loading = true;
+
+    protected override async Task OnParametersSetAsync()
+    {
+        await LoadChildren();
+    }
+
+    private async Task LoadChildren()
+    {
+        _loading = true;
+        StateHasChanged();
+        
+        if (Block != null) _children = await BlocksApi.GetChildren(Block.Id);
+        _children = _children.OrderBy(c => c.Properties.Order).ToList();
+        
+        _loading = false;
+        StateHasChanged();
+    }
+}
