ac84abb028d64a77fa78feaa05860d7baf799a08
JoaoPito <JoaoPito@github.com>
2025-01-31 20:17:18 -0300

    Revert "Moved services to the PencilCase.Web project"
    This reverts commit 772a1eeff9ae5e1f2a68d19829d17492a55f4c84.

--- PencilCase.Web/PencilCase.Web/Program.cs
+++ PencilCase.Web/PencilCase.Web/Program.cs
@@ -1,7 +1,4 @@
 using MudBlazor.Services;
-using PencilCase.Web.Client.Services.Blocks;
-using PencilCase.Web.Client.Services.LLM;
-using PencilCase.Web.Client.Services.Sources;
 using PencilCase.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,29 +9,6 @@ builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
 
 builder.Services.AddMudServices();
-
-builder.Services.AddHttpClient("BlocksAPI", client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration["BlocksAPI:url"]!);
-    client.DefaultRequestHeaders.Add("Accept", "application/json");
-});
-
-builder.Services.AddHttpClient("LlmAPI", client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration["LlmAPI:url"]!);
-    client.DefaultRequestHeaders.Add("Accept", "application/json");
-});
-
-builder.Services.AddHttpClient("SourcesAPI", client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration["SourcesAPI:url"]!);
-    client.DefaultRequestHeaders.Add("Accept", "application/json");
-});
-
-builder.Services.AddTransient<IBlocksApi, BlocksApi>();
-builder.Services.AddTransient<ILlmApi, LlmApi>();
-builder.Services.AddTransient<BlockMapper>();
-builder.Services.AddTransient<ISourcesApi, SourcesApi>();
 
 var app = builder.Build();
 
