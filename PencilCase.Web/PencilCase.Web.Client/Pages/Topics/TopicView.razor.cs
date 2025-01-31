f15d089eaca6df16b5e7d273f50e253675b7d9ce
JoaoPito <JoaoPito@github.com>
2025-01-31 19:41:30 -0300

    Added TopicView.razor.cs

---
+++ PencilCase.Web/PencilCase.Web.Client/Pages/Topics/TopicView.razor.cs
@@ -0,0 +1,242 @@
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using MudBlazorFix;
+using PencilCase.Shared.Models.Notebooks;
+using PencilCase.Web.Client.Pages.Upload;
+using PencilCase.Web.Client.Services.Blocks;
+using PencilCase.Web.Client.ViewModels;
+
+namespace PencilCase.Web.Client.Pages.Topics;
+
+public partial class TopicView : ComponentBase
+{
+    [CascadingParameter(Name = "block")] protected BlockViewModel? Block { get; set; }
+    private BlockViewModel? _oldBlock;
+    [Parameter] public IBlocksApi BlocksApi { get; set; } = null!;
+    [Parameter] public Action<BlockViewModel> RedirectTo { get; set; } = null!;
+    
+    private IEnumerable<BlockViewModel> _blockChildren = new List<BlockViewModel>();
+    
+    private bool _isLoading = false;
+    private MudTable<BlockViewModel> _blocksTable = new();
+    private BlockViewModel _rowBeforeEditing = new();
+    private record RowClickRecord(DateTime ClickTimestamp, BlockViewModel? RowClicked);
+    private RowClickRecord? _lastRowClicked = null;
+
+    protected override async Task OnParametersSetAsync()
+    {
+        if (_oldBlock != Block)
+        {
+            await LoadChildren();
+            _oldBlock = Block;
+        }
+        await base.OnParametersSetAsync();
+    }
+
+    private async Task LoadChildren()
+    {
+        _isLoading = true;
+        if (Block is not null)
+        {
+            _blockChildren = await BlocksApi.GetChildren(Block.Id);
+        }
+        _isLoading = false;
+    }
+
+    private async Task ReloadContent()
+    {
+        await LoadChildren();
+        StateHasChanged();
+    }
+    
+    // Navigation
+    
+    private async Task OnDoubleClicked(BlockViewModel block)
+    {
+        RedirectTo(block);
+    }
+
+    private async Task RowClickEvent(TableRowClickEventArgs<BlockViewModel> tableRowClickEventArgs)
+    {
+        var block = tableRowClickEventArgs.Item;
+        var clickRecord = new RowClickRecord(DateTime.Now, block);
+
+        if (block != null && _lastRowClicked != null && _lastRowClicked.RowClicked == clickRecord.RowClicked)
+        {
+            var clickDuration = clickRecord.ClickTimestamp - _lastRowClicked.ClickTimestamp;
+            if (clickDuration > TimeSpan.FromMilliseconds(100) && clickDuration < TimeSpan.FromMilliseconds(500))
+            {
+                await OnDoubleClicked(block);
+            }
+        }
+
+        _lastRowClicked = clickRecord;
+    }
+    
+    private int _selectedRowNumber = -1;
+
+    private string SelectedRowClassFunc(BlockViewModel block, int rowNumber)
+    {
+        if (_selectedRowNumber == rowNumber)
+        {
+            _selectedRowNumber = -1;
+            return string.Empty;
+        }
+        else if (_blocksTable.SelectedItem != null && _blocksTable.SelectedItem.Equals(block))
+        {
+            _selectedRowNumber = rowNumber;
+            return "selected";
+        }
+        else
+        {
+            return string.Empty;
+        }
+    }
+    
+    // Editing blocks
+    private void BackupItemBeforeEditing(Object obj)
+    {
+        var blockViewModel = (BlockViewModel)obj;
+        _rowBeforeEditing = new BlockViewModel()
+        {
+            Id = blockViewModel.Id,
+            Name = blockViewModel.Name,
+            Properties = new BlockPropertiesViewModel()
+            {
+                CreatedOn = blockViewModel.Properties.CreatedOn,
+                LastModified = blockViewModel.Properties.LastModified,
+                Order = blockViewModel.Properties.Order
+            },
+            Type = blockViewModel.Type,
+            ChildrenIds = blockViewModel.ChildrenIds,
+            ParentId = blockViewModel.ParentId
+        };
+    }
+
+    private void ResetRowToOriginalValues(Object row)
+    {
+        ((BlockViewModel)row).Id = _rowBeforeEditing.Id;
+        ((BlockViewModel)row).Name = _rowBeforeEditing.Name;
+        ((BlockViewModel)row).Type = _rowBeforeEditing.Type;
+        ((BlockViewModel)row).Properties = new BlockPropertiesViewModel()
+        {
+            CreatedOn = _rowBeforeEditing.Properties.CreatedOn,
+            LastModified = _rowBeforeEditing.Properties.LastModified,
+            Order = _rowBeforeEditing.Properties.Order
+        };
+        ((BlockViewModel)row).ChildrenIds = _rowBeforeEditing.ChildrenIds;
+        ((BlockViewModel)row).ParentId = _rowBeforeEditing.ParentId;
+    }
+
+    private void OnRowEditCommit(Object row)
+    {
+        var blockViewModel = (BlockViewModel)row;
+        BlocksApi.UpdateBlock(blockViewModel);
+    }
+
+    // Delete blocks
+    private async Task OnDeleteClicked(EditButtonContext context)
+    {
+        var block = (BlockViewModel?)context.Item;
+        if (block is not null)
+        {
+            await ShowDeleteDialog(block, DeleteBlock);
+        }
+    }
+
+    private async Task ShowDeleteDialog(BlockViewModel block, Func<Guid,Task> onAccept)
+    {
+        var options = new DialogOptions()
+        {
+            Position = DialogPosition.TopCenter
+        };
+        
+        var parameters = new DialogParameters<DeleteDialog>
+        {
+            { x => x.OnAcceptAsync, onAccept },
+            { x => x.Block, block }
+        };
+
+        await DialogService.ShowAsync<DeleteDialog>($"Delete topic", parameters, options);
+    }
+
+    private async Task DeleteBlock(Guid id)
+    {
+        var childrenList = Block?.ChildrenIds.ToList();
+        childrenList!.Remove(id);
+        Block!.ChildrenIds = childrenList;
+        
+        await BlocksApi.DeleteBlock(id);
+        await ReloadContent();
+    }
+
+    // Add blocks
+    private async Task AddNewBlockAndReload(BlockViewModel block)
+    {
+        var createdBlock = await BlocksApi.AddBlock(block);
+        
+        
+        if (block.Type is BlockType.Topic or BlockType.Source)
+        {
+            _blockChildren = _blockChildren.Append(createdBlock);
+        }
+        else if(createdBlock is not null)
+        {
+            RedirectTo(createdBlock);
+        }
+    }
+    
+    private async Task OnAddNotebookClicked()
+    {
+        _isLoading = true;
+        var newNotebook = new BlockViewModel()
+        {
+            Name = $"Untitled - {DateTime.Now.ToLocalTime():g}",
+            ParentId = Block!.Id,
+            ChildrenIds = new List<Guid>(),
+            Type = BlockType.Notebook
+        };
+
+        await AddNewBlockAndReload(newNotebook);
+        _isLoading = false;
+    }
+
+    private async Task OnAddSourceClicked()
+    {
+        _isLoading = true;
+        await OpenUploadDialogAsync();
+        _isLoading = false;
+    }
+
+    private async Task OnAddTopicClicked()
+    {
+        _isLoading = true;
+        var newTopic = new BlockViewModel()
+        {
+            Name = $"Untitled - {DateTime.Now.ToLocalTime():g}",
+            ParentId = Block!.Id,
+            ChildrenIds = new List<Guid>(),
+            Type = BlockType.Topic
+        };
+        
+        await AddNewBlockAndReload(newTopic);
+        _isLoading = false;
+    }
+    
+    private readonly DialogOptions _uploadDialogOptions = new()
+    {
+        FullScreen = false, 
+        CloseButton = true, 
+        BackdropClick = false,
+        MaxWidth = MaxWidth.ExtraLarge,
+        Position = DialogPosition.Center,
+    };
+    
+    private Task OpenUploadDialogAsync()
+    {
+        var parameters = new DialogParameters<SourceUploadDialog> { { x => x.ParentId, Block!.Id } };
+        return DialogService.ShowAsync<SourceUploadDialog>("Upload source",
+            parameters,
+            _uploadDialogOptions);
+    }
+}
