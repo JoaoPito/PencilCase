525cb4c85be53b599ada980639ebe2f271b63670
JoaoPito <JoaoPito@github.com>
2025-02-08 16:08:58 -0300

    Added migration for Block's OwnerId attribute

---
+++ PencilCase.Shared.Data/Migrations/20250208190654_Block-OwnerId.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PencilCase.Shared.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class BlockOwnerId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "OwnerId",
+                table: "Block",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Block");
+        }
+    }
+}
