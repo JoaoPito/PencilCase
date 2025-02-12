3dcaba188fe65b0696dc0defbb423b2adfed536a
JoaoPito <JoaoPito@github.com>
2025-02-12 20:58:20 -0300

    Renamed async methods on IBlocksApiEndpointsHandler.cs

--- PencilCase.API/Handlers/IBlocksApiEndpointsHandler.cs
+++ PencilCase.API/Handlers/IBlocksApiEndpointsHandler.cs
@@ -7,8 +7,8 @@ public interface IBlocksApiEndpointsHandler
 {
     IResult GetById(Guid id, ClaimsPrincipal claims);
     IResult GetAllChildren(Guid id, ClaimsPrincipal claims);
-    Task<IResult> CreateNew(BlockPostRequest request, ClaimsPrincipal claims);
-    Task<IResult> Update(Guid id, BlockPutRequest request, ClaimsPrincipal claims);
-    Task<IResult> Patch(Guid id, BlockPatchRequest request, ClaimsPrincipal claims);
-    Task<IResult> Delete(Guid id, ClaimsPrincipal claims);
+    Task<IResult> CreateNewAsync(BlockPostRequest request, ClaimsPrincipal claims);
+    Task<IResult> UpdateAsync(Guid id, BlockPutRequest request, ClaimsPrincipal claims);
+    Task<IResult> PatchAsync(Guid id, BlockPatchRequest request, ClaimsPrincipal claims);
+    Task<IResult> DeleteAsync(Guid id, ClaimsPrincipal claims);
 }
