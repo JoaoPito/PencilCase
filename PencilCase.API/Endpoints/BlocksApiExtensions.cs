f3545b1c5e62e9cfda061d1ae490036c8fb28257
JoaoPito <JoaoPito@github.com>
2025-02-12 21:25:58 -0300

    Refactored BlocksApiExtensions.cs

--- PencilCase.API/Endpoints/BlocksApiExtensions.cs
+++ PencilCase.API/Endpoints/BlocksApiExtensions.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics.SymbolStore;
 using System.Runtime.InteropServices;
 using System.Security.Claims;
 using Asp.Versioning;
 using Asp.Versioning.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Models;
+using PencilCase.API.Handlers;
 using PencilCase.Shared.Data.Database;
 using PencilCase.Shared.DTOs.Requests.Blocks;
 using PencilCase.Shared.DTOs.Responses.BlockProperties;
@@ -26,21 +28,10 @@ public static class BlocksExtensions
                     .WithTags("Blocks")
                     .RequireAuthorization();
 
-        group.MapGet("{id}", (Guid id, 
-                [FromServices] IBlocksDal dal,
-                ClaimsPrincipal claims) =>
-        {
-            var block = dal.GetBy(f => f.Id == id);
-            if (block is null){
-                return Results.NotFound();
-            }
-
-            if (ValidateOwner(block, claims))
-            {
-                return Results.Ok(MapEntityToResponse(block));
-            }
-            return Results.Unauthorized();
-        })
+        group.MapGet("{id}", (
+                Guid id, 
+                [FromServices] IBlocksApiEndpointsHandler handler,
+                ClaimsPrincipal claims) => handler.GetById(id, claims))
         .WithName("GetById")
         .WithOpenApi(x => new OpenApiOperation(x)
         {
@@ -49,21 +40,9 @@ public static class BlocksExtensions
         });
 
         group.MapGet("{id}/children", (
+                [FromServices] IBlocksApiEndpointsHandler handler,
                 Guid id, 
-                ClaimsPrincipal claims,
-                [FromServices] IBlocksDal dal) =>
-            {
-                var parent = dal.GetBy(f => f.Id == id);
-                if (parent is null) return Results.NotFound();
-
-                if (ValidateOwner(parent, claims))
-                {
-                    var childList = parent.Children.ToList();
-                    return Results.Ok(MapEntityListToResponseList(childList));
-                }
-
-                return Results.Unauthorized();
-            })
+                ClaimsPrincipal claims) => handler.GetAllChildren(id, claims))
         .WithName("GetAllChildren")
         .WithOpenApi(x => new OpenApiOperation(x)
         {
@@ -72,33 +51,10 @@ public static class BlocksExtensions
         });
 
         group.MapPost("", async (
-                [FromServices] IBlocksDal dal,
+                [FromServices] IBlocksApiEndpointsHandler handler,
                 ClaimsPrincipal claims,
-                [FromBody] BlockPostRequest request) => 
-        {
-            var parent = GetParent(request.ParentId, dal);
-            if (parent == null) return Results.BadRequest();
-            
-            if (ValidateOwner(parent, claims))
-            {
-                var newBlock = new Block();
-                var userIdClaim = claims.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
-                try
-                {
-                    newBlock = MapRequestToEntity(request, dal, parent);
-                    newBlock.OwnerId = Guid.Parse(userIdClaim);
-                }
-                catch(InvalidOperationException exc)
-                {
-                    return Results.BadRequest(new { message = exc.Message });
-                }
-
-                await dal.Add(newBlock);
-                return Results.CreatedAtRoute("GetById", new { id = newBlock.Id }, MapEntityToResponse(newBlock));
-            }
-            return Results.Unauthorized();
-        })
-        .WithName("CreateNew")
+                [FromBody] BlockPostRequest request) => await handler.CreateNewAsync(request, claims))
+        .WithName("CreateNewAsync")
         .WithOpenApi(x => new OpenApiOperation(x)
         {
             Summary = "Create a new block",
@@ -106,191 +62,27 @@ public static class BlocksExtensions
         });
 
         group.MapPut("{id}", async (Guid id, 
-                [FromServices] IBlocksDal dal, 
+                [FromServices] IBlocksApiEndpointsHandler handler, 
                 [FromBody] BlockPutRequest request,
-                ClaimsPrincipal claims) => 
-        {
-            
-            var block = dal.GetBy(b => b.Id == id);
-            if(block == null)
-                return Results.NotFound();
-
-            if (ValidateOwner(block, claims))
-            {
-                var properties = block.Properties ?? new BlockProperties();
-
-                try
-                {
-                    block.Parent = GetParent(request.ParentId, dal);
-                }
-                catch(InvalidOperationException exc)
-                {
-                    return Results.BadRequest(new { message = exc.Message });
-                }
-                try
-                {
-                    block.Name = request.Name;
-                    block.Type = request.Type;
-                    block.Parent = GetParent(request.ParentId, dal);
-                    block.ParentId = request.ParentId;
-                    properties.Order = request.Properties.Order;
-                    properties.LastModified = DateTime.UtcNow;
-                    properties.CellType = request.Properties.CellType;
-                    properties.CellShownAnswerId = request.Properties.CellShownAnswerId;
-                    block.Properties = properties;
-                    block.Children = dal.GetAllBy(b => request.ChildrenIds.Contains(b.Id)).ToList();
-                }
-                catch (NullReferenceException)
-                {
-                    return Results.BadRequest();
-                }
-
-                await dal.Update(block);
-                return Results.Ok();
-            }
-
-            return Results.Unauthorized();
-        })
-        .WithName("Update")
+                ClaimsPrincipal claims) => await handler.UpdateAsync(id, request, claims))
+        .WithName("UpdateAsync")
         .WithOpenApi(x => new OpenApiOperation(x)
         {
-            Summary = "Update Block contents.",
+            Summary = "UpdateAsync Block contents.",
             Description = "Updates block contents and properties, as well as changes the parent/child relationships."
         });
 
         group.MapPatch("{id}", async (
             Guid id, 
             ClaimsPrincipal claims,
-            [FromServices] IBlocksDal dal, 
-            [FromBody] BlockPatchRequest request) => 
-        {
-            var block = dal.GetBy(b => b.Id == id);
-            if(block == null)
-                return Results.NotFound();
-
-            if (ValidateOwner(block, claims))
-            {
-                var properties = block.Properties ?? new BlockProperties();
-                if(request.Properties != null)
-                {
-                    properties.Order = request.Properties.Order ?? properties.Order;
-                    properties.CellType = request.Properties.CellType ?? properties.CellType;
-                }
-                properties.LastModified = DateTime.UtcNow;
-                block.Properties = properties;
-
-                block.Name = request.Name ?? block.Name;
-                block.Type = request.Type ?? block.Type;
-                if(request.ParentId != null)
-                {
-                    try
-                    {
-                        block.Parent = GetParent(request.ParentId, dal);
-                    }
-                    catch(InvalidOperationException exc)
-                    {
-                        return Results.BadRequest(new { message = exc.Message });
-                    }
-                    block.ParentId = request.ParentId;
-                }
-                block.Children = request.ChildrenIds != null ? dal.GetAllBy(b => request.ChildrenIds.Contains(b.Id)).ToList() : block.Children;
-
-                await dal.Update(block);
-                return Results.Ok();
-            }
-
-            return Results.Unauthorized();
-        });
+            [FromServices] IBlocksApiEndpointsHandler handler, 
+            [FromBody] BlockPatchRequest request) => await handler.PatchAsync(id, request, claims));
 
         group.MapDelete("{id}", async (
-            [FromServices] IBlocksDal dal, 
+            [FromServices] IBlocksApiEndpointsHandler handler, 
             Guid id,
-            ClaimsPrincipal claims) => 
-        {
-            var block = dal.GetBy(b => b.Id == id);
-            if(block == null)
-                return Results.NotFound();
-            if (ValidateOwner(block, claims))
-            {
-                if(block.ParentId == null)
-                    return Results.BadRequest("Cannot delete root block!");
-            
-                await dal.Delete(block);
-                return Results.NoContent();
-            }
-
-            return Results.Unauthorized();
-        });
+            ClaimsPrincipal claims) => await handler.DeleteAsync(id, claims));
     }
 
-    static IEnumerable<BlockResponse> MapEntityListToResponseList(IEnumerable<Block> entities)
-    {
-        return entities.Select(e => MapEntityToResponse(e)).ToList();
-    }
-
-    static BlockResponse MapEntityToResponse(Block entity)
-    {
-        var properties = entity.Properties == null ? new BlockPropertiesResponse(
-            Order: 0,
-            CreatedOn: DateTime.UtcNow,
-            LastModified: DateTime.UtcNow,
-            CellType: CellType.Text,
-            CellShownAnswerId: null
-        ) :
-        new BlockPropertiesResponse(
-                Order: entity.Properties!.Order ,
-                CreatedOn: entity.Properties!.CreatedOn,
-                LastModified: entity.Properties!.LastModified,
-                CellType: entity.Properties!.CellType,
-                CellShownAnswerId: entity.Properties!.CellShownAnswerId
-            );
-
-        return new BlockResponse(
-            Id: entity.Id,
-            Name: entity.Name,
-            Type: entity.Type,
-            Properties: properties,
-            ChildrenIds: entity.Children.Select(c => c.Id).ToList(),
-            ParentId: entity.ParentId
-        );
-    }
-
-    static Block MapRequestToEntity(BlockPostRequest request, IBlocksDal dal, Block parent)
-    {
-        var block = new Block();
-
-        var properties = new BlockProperties(){
-            Parent = block,
-            ParentId = block.Id,
-            Order = request.Properties.Order,
-            CellType = request.Properties.CellType,
-            CellShownAnswerId = request.Properties.CellShownAnswerId
-        };
-
-        block.Type = request.Type;
-        block.Name = request.Name;
-        block.Properties = properties;
-        block.Children = request.ChildrenIds.Select(id => dal.GetBy(b => b.Id == id) ?? new Block()).ToList();
-        block.ParentId = request.ParentId;
-        block.Parent = parent;
-
-        return block;
-    }
     
-    static Block? GetParent(Guid? parentId, IBlocksDal dal)
-    {
-        var parent = dal.GetBy(b => b.Id == parentId);
-        if (parentId == null && parent != null)
-            throw new InvalidOperationException("Root block already exists!");
-        if (parentId != null && parent == null)
-            throw new InvalidOperationException("Parent block does not exist!");
-        return parent;
-    }
-
-    static bool ValidateOwner(Block block, ClaimsPrincipal claims)
-    {
-        var userId = claims.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
-        var blockOwnerId = block.OwnerId.ToString();
-        return (blockOwnerId != string.Empty && blockOwnerId.Equals(userId));
-    }
 }
