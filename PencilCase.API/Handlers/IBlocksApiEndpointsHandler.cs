c1c918acca643a1a7cd9dec0a28497973eae3532
JoaoPito <JoaoPito@github.com>
2025-02-19 09:39:02 -0300

    Added new GetUserRootBlockAsync method to IBlocksApiEndpointsHandler.cs

--- PencilCase.API/Handlers/IBlocksApiEndpointsHandler.cs
+++ PencilCase.API/Handlers/IBlocksApiEndpointsHandler.cs
@@ -1,4 +1,6 @@
 using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using PencilCase.Identity.Models;
 using PencilCase.Shared.DTOs.Requests.Blocks;
 
 namespace PencilCase.API.Handlers;
@@ -11,4 +13,5 @@ public interface IBlocksApiEndpointsHandler
     Task<IResult> UpdateAsync(Guid id, BlockPutRequest request, ClaimsPrincipal claims);
     Task<IResult> PatchAsync(Guid id, BlockPatchRequest request, ClaimsPrincipal claims);
     Task<IResult> DeleteAsync(Guid id, ClaimsPrincipal claims);
+    Task<IResult> GetUserRootBlockAsync(ClaimsPrincipal claims, UserManager<AppUser> userManager);
 }
