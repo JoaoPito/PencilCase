f32e5b17e7803ee5bcf94c1216341aaf4e71f2ba
JoaoPito <JoaoPito@github.com>
2025-02-19 08:31:27 -0300

    Added the root block reference to AppUser.cs

--- PencilCase.Identity/Models/AppUser.cs
+++ PencilCase.Identity/Models/AppUser.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Identity;
+using PencilCase.Shared.Models.Notebooks;
 
 namespace PencilCase.Identity.Models;
 
 public class AppUser: IdentityUser
 {
-    
+    public Guid RootBlockId { get; set; }
+    public virtual Block RootBlock { get; set; } = new();
 }
